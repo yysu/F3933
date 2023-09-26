f958607bed5234d3f09679066d30e2fea2738735
gamer00801 <91865766+gamer00801@users.noreply.github.com>
2023-09-26 14:14:15 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,46 +1,36 @@
 import getpass
 import os
-from langchain.document_loaders import PyPDFLoader
+from langchain.document_loaders import PDFPlumberLoader
 from langchain.embeddings import OpenAIEmbeddings
 from langchain.vectorstores import FAISS
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.chat_models import ChatOpenAI
-from langchain.chains import RetrievalQA
-from langchain.chains.summarize import load_summarize_chain
-from langchain.prompts import (
-    ChatPromptTemplate,
-    SystemMessagePromptTemplate,
-    HumanMessagePromptTemplate,
-)
-from langchain.memory import ConversationBufferWindowMemory
-class pdfai():
-os.environ['OPENAI_API_KEY'] = getpass.getpass('OpenAI API Key:')
-llm = ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo")
-prompt = ChatPromptTemplate(
-    messages=[
-        SystemMessagePromptTemplate.from_template(
-            "你是一個根據年報資料與上下文作回答的助手,"
-            "有明確數據可以用數據回答,"
-            "回答以繁體中文和台灣用語為主。"
-            "{context}"
-            "{history}"
-        ),
-        HumanMessagePromptTemplate.from_template("{question}")
-    ]
-    )
-memory = ConversationBufferWindowMemory(k=2, return_messages=True,input_key='question')
-chain_type_kwargs = {'prompt':prompt, 'memory':memory}
-def pdf_loader(file,size,overlap):
-    loader =  PyPDFLoader(file)
-    doc = loader.load()
-    text_splitter = RecursiveCharacterTextSplitter(chunk_size=size, chunk_overlap=overlap)
-    new_doc = text_splitter.split_documents(doc)
-    db = FAISS.from_documents(new_doc, OpenAIEmbeddings())
-    return db
-def question_and_answer(retriever,q):
-    qa = RetrievalQA.from_chain_type(llm=llm, chain_type="stuff",
-                                    retriever=retriever,
-                                    return_source_documents=True,
-                                    chain_type_kwargs=chain_type_kwargs)
-    result = qa(q)
-    return result
+from langchain.prompts import ChatPromptTemplate
+from langchain.chains import LLMChain
+class PdfLoader:
+    def __init__(self):
+        os.environ['OPENAI_API_KEY'] = getpass.getpass('OpenAI API Key:')
+        self.llm = ChatOpenAI(temperature=0, model="gpt-3.5-turbo-16k")
+        self.data_prompt=ChatPromptTemplate.from_messages(messages=[
+            ("system","你現在是一位專業的證券分析師,"
+            "你會統整年報並進行分析, 針對{output}作分析, 然後生成一份專業的趨勢分析報告。"),
+            ("human","{text}")])
+        self.data_chain = LLMChain(llm=self.llm, prompt=self.data_prompt)
+        self.word_prompt=ChatPromptTemplate.from_messages(messages=[
+            ("system","你可以將使用者輸入的句子取出一個關鍵字,"
+            "要取出的關鍵字會是以年報中的會出現的相關名詞為主"),
+            ("human","{input}")])
+        self.word_chain = LLMChain(llm=self.llm, prompt=self.word_prompt)
+    def pdf_loader(self,file,size,overlap):
+        loader = PDFPlumberLoader(file)
+        doc = loader.load()
+        text_splitter = RecursiveCharacterTextSplitter(chunk_size=size,
+                                                chunk_overlap=overlap)
+        new_doc = text_splitter.split_documents(doc)
+        db = FAISS.from_documents(new_doc, OpenAIEmbeddings())
+        return db
+    def analyze_chain(self,db,input):
+        data = db.max_marginal_relevance_search(input)
+        word = self.word_chain.run(input)
+        result = self.data_chain({'output':word,'text':data})
+        return result['text']
