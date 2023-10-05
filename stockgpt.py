f8463874d1508d003c5c3c5188d4755e117dbe69
gamer00801 <91865766+gamer00801@users.noreply.github.com>
2023-10-05 14:27:37 +0800

    Update stockgpt.py

--- stockgpt.py
+++ stockgpt.py
@@ -54,7 +54,7 @@ class StockAnalysis():
   
     data = {
       '日期': df.index.strftime('%Y-%m-%d').tolist(),
-      '收盤價': df['收盤價'].tolist(),
+      # '收盤價': df['收盤價'].tolist(),
       '每日報酬': df['收盤價'].pct_change().tolist(),
       '漲跌價差': df['調整後收盤價'].diff().tolist()
       }
