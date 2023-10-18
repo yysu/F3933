0f49d2ef5b012ff766acf7d02359e2aad1b564e9
gamer00801 <91865766+gamer00801@users.noreply.github.com>
2023-10-18 17:38:55 +0800

    Update Stock_Loader.py

--- Stock_Loader.py
+++ Stock_Loader.py
@@ -19,7 +19,7 @@ class PdfLoader:
         self.llm = ChatOpenAI(temperature=0, model="gpt-3.5-turbo-16k")
         self.data_prompt=ChatPromptTemplate.from_messages(messages=[
             ("system","你現在是一位專業的證券分析師,"
-            "你會統整年報並進行分析, 針對{output}作分析, 然後生成一份專業的趨勢分析報告。"),
+            "你會詳細、嚴謹的統整年報並進行分析, 針對{output}作分析, 提及重要的數字, 然後生成一份專業的趨勢分析報告,tokens的上限為1600。"),
             ("human","{text}")])
         self.data_chain = LLMChain(llm=self.llm, prompt=self.data_prompt)
         self.word_prompt=ChatPromptTemplate.from_messages(messages=[
