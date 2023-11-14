099938bc8a2dbbe38c9e192c73cb4bdbf3a6f387
gamer00801 <91865766+gamer00801@users.noreply.github.com>
2023-11-14 16:07:27 +0800

    Update Stock_DB.py

--- Stock_DB.py
+++ Stock_DB.py
@@ -259,7 +259,8 @@ class StockDB:
         # 重新排列列的顺序
         combined_df = combined_df[['年份', '季度', '營業收入', '營業費用', '稅後淨利', '每股盈餘']]
         combined_df.insert(0, '股號', id)   # 加入股號欄
-        combined_df.to_sql('季頻', self.conn, if_exists='append', index=False)
+        # combined_df.to_sql('季頻', self.conn, if_exists='append', index=False)
+        combined_df.to_sql('季頻', self.conn, if_exists='replace', index=False)
         
     return print("更新完成")
 
